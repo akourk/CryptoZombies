156eed2cada2a365f101ea3954c1b810fa20107a
akourk <alexkourks@gmail.com>
2018-06-21 14:58:44 -0700

    Practicing with Solidity creating pseudo-randomly generated zombies
    Practicing with Solidity, learning syntax, naming conventions, etc. The program uses a user-inputted name to generate a pseudo-random integer by using the hash function 'keccak256' (a version of SHA3). This random number is then used as 'DNA' for zombies to make them relatively unique. Then uses javascript ethereum library, Web3.js, to interact with the Solidity contract. The javascript takes the randomly generated values to swap out images and apply CSS filters.

---
+++ Lesson1/CryptoZombies1.sol
@@ -0,0 +1,64 @@
+// this program uses an inputted name to generate pseudo-random dna/features of a zombie
+// the purpose is to learn Solidity
+//	naming conventions, syntax, etc.
+// this is the bare version, stripped of notes.
+// easier to read
+
+pragma solidity ^0.4.19
+
+contract ZombieFactory {
+
+	event NewZombie(uint zombieId, string name, uint dna);
+
+	uint dnaDigits = 16; 
+	uint dnaModulus = 10 ** dnaDigits;
+
+	struct Zombie {
+		uint dna;
+		string name;
+	}
+
+	Zombie[] public zombies;
+	
+	function _createZombies(string _name, uint _dna) private {
+		uint id = zombies.push(Zombie(_name, _dna)) - 1;
+		NewZombie(id, _name, _dna);
+	}
+
+	function _generateRandomDna(string _str) private view returns (uint) {
+		uint rand = uint(keccak256(_str));    
+		return rand % dnaModulus;
+	}
+
+	function createRandomZombie(string _name) public {
+		uint randDna = _generateRandomDna(_name);
+		_createZombie(_name, randDna);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
